5d34055f59abada0a43374675d43cb570612cb11
ArnavSwami <arnavswami03@gmail.com>
2024-04-27 16:40:14 -0700

    Added sign up and changed layout components

---
+++ src/app/signUp/page.js
@@ -0,0 +1,19 @@
+"use client";
+import React, { useState } from "react";
+import { auth, app } from "../../../firebase-config";
+import { createUserWithEmailAndPassword } from "firebase/auth";
+import { getFirestore, doc, setDoc } from "firebase/firestore";
+import { Nova_Flat } from "next/font/google";
+
+const db = getFirestore(app);
+
+function signUp() {
+    const [email, setEmail] = useState("");
+    const [oassword, setPassword] = usesState("");
+    const [year, setYear] = usesState("");
+    const [weight, setWeight] = usesState("");
+    const [name, setName] = usesState("");
+    const [error, setError] = usesState("");
+    
+
+}

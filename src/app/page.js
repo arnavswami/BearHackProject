77e0d06194f4338c08aa3d545d46f9480b8dc49e
ArnavSwami <arnavswami03@gmail.com>
2024-04-27 20:28:30 -0700

    New message added

--- src/app/page.js
+++ src/app/page.js
@@ -1,7 +1,7 @@
 import React from "react";
 
 const Home = () => {
-  return <>Hello World</>;
+  return <>Welcome to your personal health and wellness progress checkup</>;
 };
 
 export default Home;
